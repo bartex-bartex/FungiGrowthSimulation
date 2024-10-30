bbfc61875162efe3af8425a834852a47c427006d
Bartosz Warchoł <bartex132@gmail.com>
2024-10-30 21:04:46 +0100

    Add scaling

--- app.py
+++ app.py
@@ -8,22 +8,25 @@ def frame_to_surface(frame: np.array) -> pygame.Surface:
     frame = np.where(frame, 255, 0).astype(np.uint8)
     return pygame.surfarray.make_surface(frame)
 
+def display_frame(frame):
+    frame_surface = frame_to_surface(frame)
+    scaled_frame_surface = pygame.transform.scale(frame_surface, (display_width, display_height))
+    screen.blit(scaled_frame_surface, (0, 0))
+    pygame.display.flip()
 
-height = 320
-width = 320
-MAX_GENERATIONS = 400
+height, width = 300, 300
+display_height, display_width = 600, 600
+MAX_GENERATIONS = 4000
 
 pygame.init()
 pygame.display.set_caption("Fungi Growth Simulation")
 
-screen = pygame.display.set_mode((height, width))  # surface that is main window default black
+screen = pygame.display.set_mode((display_height, display_width))  # surface that is main window default black
 clock = pygame.time.Clock()
+model = Model(height, width, growth_probabilities.probability_3)
 
-model = Model(height, width, growth_probabilities.probability_2)
 frame = model.saw_spore()
-
-screen.blit(frame_to_surface(frame), (0, 0))
-pygame.display.flip()
+display_frame(frame)
 
 frames = []
 frames.append(np.where(frame, 255, 0).astype(np.uint8))
@@ -36,16 +39,15 @@ while running:
             running = False
 
     frame = model.next_frame()
-    screen.blit(frame_to_surface(frame), (0, 0))
-    pygame.display.flip()
-    frames.append(np.where(frame, 255, 0).astype(np.uint8))
+    display_frame(frame)
 
     frame_id = model.get_generation()
     print(frame_id)
 
-
     if frame_id > MAX_GENERATIONS:
         running = False
+
+    frames.append(np.where(frame, 255, 0).astype(np.uint8))
 
     # clock.tick(100)
 
